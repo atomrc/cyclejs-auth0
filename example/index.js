5bd8a1ec9f1466cc1dbbfedb65ac1a048ca391b3
Thomas Belin <thomasbelin4@gmail.com>
2016-08-15 20:15:05 +0200

    Improve example

--- example/index.js
+++ example/index.js
@@ -1,37 +1,21 @@
 import {run} from "@cycle/xstream-run";
 
 import {makeAuth0Driver, protect} from "cyclejs-auth0";
-import {makeRouterDriver} from 'cyclic-router'
 import {makeDOMDriver, div} from "@cycle/dom";
-import {createHistory} from 'history';
 
-function App({ auth0 }) {
-
-    const token$ = auth0.token$;
+function App({ props }) {
 
     return {
-        DOM: token$
-            .map(token => token ? div("logged") : div("please log in")),
-        auth0: token$
-            .filter(token => !token)
-            .mapTo({ action: "show" })
+        DOM: props
+            .token$
+            .debug("app token")
+            .map(token => token ? div("logged") : div("please log in"))
     }
-}
-
-function main(sources) {
-
-    const app = protect(App)(sources);
-
-    return {
-        DOM: app.DOM,
-        auth0: app.auth0
-    };
 }
 
 var drivers = {
     DOM: makeDOMDriver("#main", { transposition: true }),
-    auth0: makeAuth0Driver("yKJO1ckwuY1X8gPEhTRfhJXyObfiLxih", "mdocs.auth0.com"),
-    router: makeRouterDriver(createHistory())
+    auth0: makeAuth0Driver("tDjcxZrzyKB8a5SPqwn4XqJfdSvW4FXi", "atomrc.eu.auth0.com")
 };
 
-run(main, drivers);
+run(protect(App), drivers);
