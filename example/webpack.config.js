cec66ded0e7cfb38a3c2fc81693ef2af82d7100f
Thomas Belin <thomasbelin4@gmail.com>
2016-08-19 20:29:13 +0200

    remove useless webpack config and dependencies

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -8,18 +8,6 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /node_modules[\\\/]auth0-lock[\\\/].*\.js$/, 
-                loaders: [ 'transform-loader/cacheable?brfs', 'transform-loader/cacheable?packageify' ]
-            },
-            {
-                test: /node_modules[\\\/]auth0-lock[\\\/].*\.ejs$/, 
-                loader: 'transform-loader/cacheable?ejsify'
-            },
-            {
-                test: /\.json$/, 
-                loader: 'json'
-            },
-            {
                 test: /\.js/, 
                 exclude: /node_modules/,
                 loader: 'babel-loader'
